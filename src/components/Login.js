6e050ee1e71d01caabaab63ca70670da16959647
Manan Jain <manan.nitdl@gmail.com>
2023-04-08 23:30:31 +0530

    my updates

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import { auth } from "../firebase";
 
 const Login = () => {
@@ -21,45 +22,52 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Login</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={login}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-                value={email}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-                value={password}
-                required
-              />
-            </Form.Group>
-            <Button className="w-100" type="submit">
-              Login
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Dont have an account?
-        <Link to="/signup">Signup</Link>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <div>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Login</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={login}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                    value={email}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(event) => {
+                      setPassword(event.target.value);
+                    }}
+                    value={password}
+                    required
+                  />
+                </Form.Group>
+                <Button className="w-100" type="submit">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Dont have an account?
+            <Link to="/signup">Signup</Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase";
+import { Container } from "react-bootstrap";
 import { db } from "../firebase";
 import { addDoc, collection } from "firebase/firestore";
 import DetailsUser from "./DetailsUser";
@@ -55,142 +56,149 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={register}>
-            <Form.Group id="fname">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={(event) => {
-                  setFname(event.target.value);
-                }}
-                value={fName}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="lname">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={(event) => {
-                  setLname(event.target.value);
-                }}
-                value={lName}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-                value={email}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="phone_number">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control
-                type="tel"
-                onChange={(event) => {
-                  setPhoneNum(event.target.value);
-                }}
-                value={phoneNum}
-                maxLength="10"
-                required
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-                value={password}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                onChange={(event) => {
-                  setPasswordConfirm(event.target.value);
-                }}
-                value={passwordConfirm}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="bank_acc_no">
-              <Form.Label>Bank Account Number</Form.Label>
-              <Form.Control
-                type="number"
-                onChange={(event) => {
-                  setBankAccNo(event.target.value);
-                }}
-                value={bankAccNo}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="isfc_code">
-              <Form.Label>ISFC Code</Form.Label>
-              <Form.Control
-                type="string"
-                onChange={(event) => {
-                  setIsfcCode(event.target.value);
-                }}
-                value={isfcCode}
-                required
-              />
-            </Form.Group>
-            <Form.Group id="select_role">
-              <Form.Label>Select role</Form.Label>
-              <Form.Select
-                id="dropdown"
-                aria-label="Select Role"
-                onChange={(event) => {
-                  setCurrentUser(event.target.value);
-                }}
-                value={currentUser}
-              >
-                <option value="Parent">Parent</option>
-                <option value="Merchant">Merchant</option>
-              </Form.Select>
-            </Form.Group>
-            <br />
-            {currentUser === "Merchant" ? (
-              <Form.Group id="gstin">
-                <Form.Label>GSTIN</Form.Label>
-                <Form.Control
-                  type="string"
-                  onChange={(event) => {
-                    setGstin(event.target.value);
-                  }}
-                  value={gstin}
-                  required
-                />
-              </Form.Group>
-            ) : (
-              <></>
-            )}
-            <br />
-            <Button className="w-100" type="submit">
-              Signup
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Login</Link>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <div>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Sign Up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={register}>
+                <Form.Group id="fname">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={(event) => {
+                      setFname(event.target.value);
+                    }}
+                    value={fName}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="lname">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={(event) => {
+                      setLname(event.target.value);
+                    }}
+                    value={lName}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                    value={email}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="phone_number">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    onChange={(event) => {
+                      setPhoneNum(event.target.value);
+                    }}
+                    value={phoneNum}
+                    maxLength="10"
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(event) => {
+                      setPassword(event.target.value);
+                    }}
+                    value={password}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password-confirm">
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control
+                    type="password"
+                    onChange={(event) => {
+                      setPasswordConfirm(event.target.value);
+                    }}
+                    value={passwordConfirm}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="bank_acc_no">
+                  <Form.Label>Bank Account Number</Form.Label>
+                  <Form.Control
+                    type="number"
+                    onChange={(event) => {
+                      setBankAccNo(event.target.value);
+                    }}
+                    value={bankAccNo}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="isfc_code">
+                  <Form.Label>ISFC Code</Form.Label>
+                  <Form.Control
+                    type="string"
+                    onChange={(event) => {
+                      setIsfcCode(event.target.value);
+                    }}
+                    value={isfcCode}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="select_role">
+                  <Form.Label>Select role</Form.Label>
+                  <Form.Select
+                    id="dropdown"
+                    aria-label="Select Role"
+                    onChange={(event) => {
+                      setCurrentUser(event.target.value);
+                    }}
+                    value={currentUser}
+                  >
+                    <option value="Parent">Parent</option>
+                    <option value="Merchant">Merchant</option>
+                  </Form.Select>
+                </Form.Group>
+                <br />
+                {currentUser === "Merchant" ? (
+                  <Form.Group id="gstin">
+                    <Form.Label>GSTIN</Form.Label>
+                    <Form.Control
+                      type="string"
+                      onChange={(event) => {
+                        setGstin(event.target.value);
+                      }}
+                      value={gstin}
+                      required
+                    />
+                  </Form.Group>
+                ) : (
+                  <></>
+                )}
+                <br />
+                <Button className="w-100" type="submit">
+                  Signup
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Already have an account? <Link to="/login">Login</Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
