854dd43092a876120010c5ae942ddbd3ce721363
thisispriyanshu <priyanshuagrawal801@gmail.com>
2023-04-08 19:04:30 +0530

    barcode scanner and generator added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import DetailsUser from "./components/DetailsUser";
-
 import { Container } from "react-bootstrap";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import BarcodeScanner from "./components/BarcodeScanner";
+import BarcodeGenerator from "./components/BarcodeGenerator";
 
 const App = () => {
   return (
@@ -21,6 +21,8 @@ const App = () => {
           {/* <ToastContainer /> */}
           <BrowserRouter>
             <Routes>
+              <Route path='/bscan' Component={BarcodeScanner}/>
+              <Route path='/bgenerate' Component={BarcodeGenerator}/>
               <Route path="/signup" Component={Signup} />
               <Route path="/login" Component={Login} />
               <Route path="/dashboard" Component={Dashboard} />
